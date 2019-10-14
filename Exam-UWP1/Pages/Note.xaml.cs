13b4e868bdc5415d5c1f840cf2916e78a9db0c19
hoanghai94 <hoanghai.yps@gmail.com>
2019-10-14 20:04:43 +0700

    exam

---
+++ Exam-UWP1/Pages/Note.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Exam_UWP1.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Note : Page
+    {
+        public Note()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            string noteContent = this.note.Text;
+            SaveNoteToLocalStorage(noteContent);
+        }
+
+        private void SaveNoteToLocalStorage(string noteContent)
+        {
+            var fileName = DateTime.Now.ToString("yyyy-MM-dd-hh-mm");
+
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile sampleFile = storageFolder.CreateFileAsync(fileName + ".txt",
+                Windows.Storage.CreationCollisionOption.ReplaceExisting).GetAwaiter().GetResult();
+            Windows.Storage.FileIO.WriteTextAsync(sampleFile, noteContent).GetAwaiter().GetResult();
+        }
+
+        private void ReadFileFromLocalStorage()
+        {
+
+        }
+    }
+}
